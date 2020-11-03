31aced049eeca649959ef6b8d728502543b6bac1
swapnil7000 <swapnil.bhojwani@gmail.com>
2020-11-03 17:21:05 +0530

    Comments updated

--- app/src/main/java/com/example/livecoding/WeightPicker.java
+++ app/src/main/java/com/example/livecoding/WeightPicker.java
@@ -17,7 +17,7 @@ class WeightPicker {
      * There are 9 TODOs in this file, locate them using the window for it given at the bottom.
      * Also, complete them in order.
      * <p>
-     * TODO 1 : Design layout weight_picker_dialog.xml for this WeightPicker Dialog
+     * : Design layout weight_picker_dialog.xml for this WeightPicker Dialog
      * with 2 NumberPickers (for kg & g)
      */
 
@@ -32,11 +32,11 @@ class WeightPicker {
                 .setPositiveButton("SELECT", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-                        //TODO 3 : Replace 0s & assign kg & g values from respective NumberPickers
+                        // Replace 0s & assign kg & g values from respective NumberPickers
                         int kg = b.numberPickerKg.getValue();
                         int g = b.numberPickerg.getValue() * 50;
                         Toast.makeText(context, "" + g, Toast.LENGTH_SHORT).show();
-                        //TODO 4 : Add GuardCode to prevent user from selecting 0kg 0g. If so, then return
+                        // Add GuardCode to prevent user from selecting 0kg 0g. If so, then return
                         if (kg == 0 && g == 0) {
                             return;
                         }
@@ -53,19 +53,18 @@ class WeightPicker {
 
         setupNumberPickers(b.numberPickerKg, b.numberPickerg);
 
-        //TODO 5 : Call new WeightPicker().show() in MainActivity and pass (this, new OnWeight...)
+        // Call new WeightPicker().show() in MainActivity and pass (this, new OnWeight...)
 
-        //TODO 6 : Show toast of selected weight in onWeightPicked() method
+        //Show toast of selected weight in onWeightPicked() method
 
-        //TODO 7 : Find appropriate solution for : NumberPicker not formatting the first item
+        //Find appropriate solution for : NumberPicker not formatting the fi
+        //Test your code :)
 
-        //TODO 8 : Test your code :)
-
-        //TODO 9 : Try to understand the flow as to how our Listener interface is working
+        //Try to understand the flow as to how our Listener interface is working
     }
 
     private void setupNumberPickers(NumberPicker numberPickerKg, NumberPicker numberPickerg) {
-        //TODO 2 : Define this method to setup kg & g NumberPickers as per the given ranges
+        //Define this method to setup kg & g NumberPickers as per the given ranges
         //kg Range - 0kg to 10kg
         //g Range - 0g to 950g
         numberPickerg.setMinValue(0);
@@ -84,11 +83,6 @@ class WeightPicker {
                 return value + " " + "kg";
             }
         });
-
-        View firstItem = numberPickerg.getChildAt(0);
-        if (firstItem != null) {
-            firstItem.setVisibility(View.INVISIBLE);
-        }
         View firstItemKg = numberPickerKg.getChildAt(0);
         if (firstItemKg != null) {
             firstItemKg.setVisibility(View.INVISIBLE);
